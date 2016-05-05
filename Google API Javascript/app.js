a341cf1fe0c0e41b7284bb239fa3199d8dcb1839
Bikram Baral <baralbikram@hotmail.com>
2016-05-04 20:39:36 -0700

    Added google API

---
+++ Google API Javascript/app.js
@@ -0,0 +1,16 @@
+ var http = require('http'),
+    fs = require('fs');
+
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8000;
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(port);
+    console.log("server listening at " + hostname + ":" + port);
+});
